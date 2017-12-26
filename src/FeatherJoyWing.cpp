877f37f38cdbf7373ba319a17d36d595a12443ca
Leonid Lezner <leonidlezner@Leonids-MBP.fritz.box>
2017-12-26 13:19:10 +0100

    First version of the FeatherJoyWing class

---
+++ src/FeatherJoyWing.cpp
@@ -0,0 +1,108 @@
+#include "FeatherJoyWing.h"
+
+static uint32_t FeatherJoyWing_Button_Mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
+                       (1 << BUTTON_LEFT) | (1 << BUTTON_UP) | (1 << BUTTON_SEL);
+
+
+FeatherJoyWing::FeatherJoyWing(Adafruit_seesaw &seasaw):
+    ss(seasaw), joystickCallback(NULL), buttonCallback(NULL),
+    joystick_zero_threshold(5), joystick_react_threshold(2)
+{
+    for(int i = 0; i < FJ_NUM_OF_BUTTONS; i++)
+    {
+        buttons[i].pressed = false;
+        buttons[i].hasChanged = false;
+    }
+
+    buttons[0].pinId = BUTTON_RIGHT;
+    buttons[1].pinId = BUTTON_DOWN;
+    buttons[2].pinId = BUTTON_LEFT;
+    buttons[3].pinId = BUTTON_UP;
+    buttons[4].pinId = BUTTON_SEL;
+}
+
+bool FeatherJoyWing::begin()
+{
+    if (!this->ss.begin(JOYWING_ADDRESS))
+    {
+        Serial.println("JOYWING ERROR!");
+        return false;
+    }
+
+    this->ss.pinModeBulk(FeatherJoyWing_Button_Mask, INPUT_PULLUP);
+
+    this->ss.setGPIOInterrupts(FeatherJoyWing_Button_Mask, true);
+    
+    pinMode(FEATHERJOYWING_IRQ_PIN, INPUT);
+
+    return true;
+}
+
+bool FeatherJoyWing::update()
+{
+    static int16_t last_x = 0, last_y = 0;
+
+    if(NULL != this->joystickCallback)
+    {
+        // x: -128...0...128. y: 128...0...-128
+        int16_t x = ss.analogRead(JOYSTICK_H) / 4 - 128;
+        int16_t y = 128 - ss.analogRead(JOYSTICK_V) / 4;
+
+        if(abs(x) < this->joystick_zero_threshold)
+        {
+            x = 0;
+        }
+
+        if(abs(y) < this->joystick_zero_threshold)
+        {
+            y = 0;
+        }
+
+        if ((abs(x - last_x) > this->joystick_react_threshold)  
+            ||  (abs(y - last_y) > this->joystick_react_threshold))
+        {
+            this->joystickCallback(x, y);
+
+            last_x = x;
+            last_y = y;
+        }
+    }
+
+    if(NULL != this->buttonCallback)
+    {
+
+        if(!digitalRead(FEATHERJOYWING_IRQ_PIN))
+        {
+            uint32_t buttons_state = this->ss.digitalReadBulk(FeatherJoyWing_Button_Mask);
+            
+            for(int i = 0; i < FJ_NUM_OF_BUTTONS; i++)
+            {
+                bool new_state = false;
+
+                if(!(buttons_state & (1 << this->buttons[i].pinId)))
+                {
+                    new_state = true;
+                }
+                else
+                {
+                    new_state = false;
+                }
+
+                if(new_state != this->buttons[i].pressed)
+                {
+                    this->buttons[i].hasChanged = true;
+                }
+                else
+                {
+                    this->buttons[i].hasChanged = false;
+                }
+
+                this->buttons[i].pressed = new_state;
+            }
+            
+            this->buttonCallback(this->buttons, FJ_NUM_OF_BUTTONS);
+        }
+    }
+
+    return true;
+}
---
+++ src/FeatherJoyWing.h
@@ -0,0 +1,52 @@
+#ifndef LIB_FEATHERJOYWING_H
+#define LIB_FEATHERJOYWING_H
+
+#include "Adafruit_seesaw.h"
+
+#ifndef JOYWING_ADDRESS
+#define JOYWING_ADDRESS SEESAW_ADDRESS
+#endif
+
+#define FJ_NUM_OF_BUTTONS   5
+#define BUTTON_RIGHT        6
+#define BUTTON_DOWN         7
+#define BUTTON_LEFT         9
+#define BUTTON_UP           10
+#define BUTTON_SEL          14
+
+#define JOYSTICK_H      3
+#define JOYSTICK_V      2
+
+#ifndef FEATHERJOYWING_IRQ_PIN
+#define FEATHERJOYWING_IRQ_PIN         6
+#endif
+
+typedef struct __fj_button__
+{
+    uint8_t pinId;
+    bool pressed;
+    bool hasChanged;
+} FJBUTTON;
+
+typedef void (*featherjoywing_joystick_callback) (int8_t, int8_t);
+typedef void (*featherjoywing_button_callback) (FJBUTTON*, uint8_t);
+
+class FeatherJoyWing
+{
+    public:
+        FeatherJoyWing(Adafruit_seesaw &ss);
+        ~FeatherJoyWing() {};
+        bool begin();
+        bool update();
+        void registerJoystickCallback(featherjoywing_joystick_callback callback) { this->joystickCallback = callback; };
+        void registerButtonCallback(featherjoywing_button_callback callback) { this->buttonCallback = callback; };
+    private:
+        Adafruit_seesaw &ss;
+        featherjoywing_joystick_callback joystickCallback;
+        featherjoywing_button_callback buttonCallback;
+        uint8_t joystick_zero_threshold;
+        uint8_t joystick_react_threshold;
+        FJBUTTON buttons[FJ_NUM_OF_BUTTONS];
+};
+
+#endif
